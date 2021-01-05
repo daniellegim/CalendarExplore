73b50fef7446f4182d66f62a44eb10ba21b2369c
Danielle Gimpel <danielle2581@gmail.com>
2021-01-05 17:10:29 +0200

    calendar

--- src/CalendarComp.js
+++ src/CalendarComp.js
@@ -1,30 +1,61 @@
 import React, {useState} from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-// import { Calendar } from '@fullcalendar/core'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import "react-big-calendar/lib/addons/dragAndDrop/styles.css"
-// import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop"
-import moment from 'moment'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
-const localizer = momentLocalizer(moment)
-// const DnDCalendar = withDragAndDrop(Calendar);
+// import { Calendar, momentLocalizer } from 'react-big-calendar'
+// import 'react-big-calendar/lib/css/react-big-calendar.css'
+// import "react-big-calendar/lib/addons/dragAndDrop/styles.css"
+// import FullCalendar from 'fullcalendar-reactwrapper'
+// import 'fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
+// import moment from 'moment'
+// import he from 'moment/locale/he.js'
+
+//const localizer = momentLocalizer(moment)
 
 function CalendarComp() {
-    const myEventsList = [{
-        start: moment().toDate(),
-        end: moment().toDate(),
-        title: "Live shooting"
-    }]
+    const events = [
+        { title: 'מטווחים חיים', date: '2021-01-05' },
+        { title: 'מטווחי לייזר', date: '2021-01-10' }
+    ]
 
     return(
-        <Calendar
-            localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500 }}
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            headerToolbar={{start: 'dayGridMonth,dayGridWeek,dayGridDay', 
+                            center: 'title' ,
+                            end: 'prev,next today'}}
+            buttonText={{today: 'היום',
+                         dayGridMonth: 'חודש',
+                         dayGridWeek: 'שבוע',
+                         dayGridDay: 'יום'}}
+            events={events}
+            locale='he'
+            height='auto'
+            direction='rtl'
         />
+
+        // <Calendar
+        //     localizer={localizer}
+        //     events={myEventsList}
+        //     rtl='true'
+        //     culture='he'
+        //     views={views}
+        //     style={{ height: 500 }}
+        // />
+
+        // <FullCalendar
+        //     header = {{
+        //         left: 'today myCustomButton prev,next',
+        //         center: 'title',
+        //         right: 'agenda,basicDay,basicWeek,month'
+        //     }}
+        //     //navLinks= {true} // can click day/week names to navigate views
+        //     editable= {true}
+        //     eventLimit= {true} // allow "more" link when too many events
+        //     events = {events} 
+        //     locale='he'
+        //     buttonText={{ today: 'היום', month:'חודש' , basicWeek: 'שבוע', basicDay: 'יום', agenda: 'סדר יום'}}
+        // />
     )
 }
 
